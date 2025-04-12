2e4b389174d65589ea1ca2ea27e329cb40fbecf6
Test Test <test@test.com>
2025-04-12 02:03:41 +0200

    Added first e2e implementation which does the visualization.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from agent import AbstractionAgent
+from agent import AbstractionAgent, Component
 from static_analyzer import Analyzer
 import os
 
@@ -16,8 +16,19 @@ result = md.convert("./resources/test.xlsx")
     cfg_str, groups, nodes, edges = stat_analyzer.analyze()
 
     agent = AbstractionAgent("MarkItDown")
-    print(agent.get_interesting_modules(cfg_str, nodes))
-
+    abstract_modules = agent.get_interesting_modules(cfg_str, nodes)
+    mermaid_str = "flowchart LR\n"
+    modules = abstract_modules['interesting_modules']
+    for id in range(len(modules) - 1):
+        # For now I will just do a naive diagram generation
+        node, next_node  = modules[id], modules[id + 1]
+        node, next_node = Component.model_validate(node), Component.model_validate(next_node)
+        descr = node.description.replace("(", "").replace(")", "")
+        next_descr = next_node.description.replace("(", "").replace(")", "")
+        mm_string = f"    {node.name}[<b>{node.name}</b><br><i>{descr}</i>] -- {node.communication} --> {next_node.name}[<b>{next_node.name}</b><br><i>{next_descr}</i>]\n"
+        mermaid_str += mm_string
+    with open("markitdown_diagram.md", "w") as f:
+        f.write(mermaid_str)
 
 if __name__ == "__main__":
     main()
--- prompts.py
+++ prompts.py
@@ -5,6 +5,9 @@ The project I am working on is called {project_name}. It is a Python project and
 Please analyze and give me the high-level abstraction of the code in terms of main components and steps.
 This is the CFG:
 {cfg_str}
+
+Please provide the abstraction in the following format:
+{format_instructions}
 """
 
 
@@ -13,16 +16,16 @@ You are the lead software engineer of a software project. Your task is to analyz
 
 Your analysis will follow 3 clear steps:
 1. **Identify and list relevant modules, functions, and classes** from the control flow graph. Abstract away low-level or utility components not important for high-level understanding.
-2. **Use tools only as needed** to gather source code and extract meaningful summaries. For each important module, fetch the code **once** and summarize its core logic.
+2. **Use tools only as needed** to gather source code and extract meaningful summaries. For each important module, function or class, fetch the code **once** and summarize its core logic.
 3. **Output a final summary** with the main components in the following format:
 
-1. Entry point is `<entry_point>` which does <...>. It then calls 2. `<module_name>` which does <...>.
-2. `<module_name>` is the module which does <...>.
+1. Entry point is `<entry_point>` which does <...>. It then calls 2. `<module_name>` with <...>
+2. `<module_name>` is the module which does <...>. It then calls 3. `<module_name>` with <...>
 3. ...
 
 ### Rules:
 - Only use the tool `read_module_tool` when the module is unclear and worth deeper inspection.
 - Do not use the tool more than once per module.
-- After inspecting up to 3–5 core modules, finalize the summary.
+- After inspecting up to 5–10 core modules, finalize the summary.
 - Your output should end with the final structured component list.
 """
