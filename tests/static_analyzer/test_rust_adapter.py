75f9a8beee20db3dac1c5df4143e23b9b97cce4e
ivanmilevtues <ivanmilevtues@gmail.com>
2026-04-12 00:55:20 -0700

    Revert "Fix Rust diagnostics: send didSave to trigger cargo check"
    This reverts commit 62483fe601169ea501e07447694aed5f389fc38e.

--- tests/static_analyzer/test_rust_adapter.py
+++ tests/static_analyzer/test_rust_adapter.py
@@ -128,11 +128,6 @@ class TestWaitForDiagnostics:
         events: list = []
 
         class FakeClient:
-            opened_uris: set[str] = set()
-
-            def did_save(self, file_path):
-                events.append("save")
-
             def reset_ready_signal(self):
                 events.append("reset")
 
@@ -155,11 +150,6 @@ class TestWaitForDiagnostics:
         events: list = []
 
         class FakeClient:
-            opened_uris: set[str] = set()
-
-            def did_save(self, file_path):
-                events.append("save")
-
             def reset_ready_signal(self):
                 events.append("reset")
 
